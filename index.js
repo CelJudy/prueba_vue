e9e17856519bf837ac765756a23fe4e34f7b8cd3
CelJudy <celjudygmail.com>
2024-10-20 13:06:46 -0600

    otra prueba

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 
+//endpoits
+
 app.post("/api/requerimiento",(req,res)=>{
     console.log(req.body);
     res.json({mensaje:"ok"});
@@ -16,11 +18,15 @@ app.get("/api/numero/:numero",(req,res)=>{
     res.json({mensaje:"oki"});
 });
 
+//rutas de vue
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+
+//------------
 
 app.listen(3000, (error)=>{
     console.log("APIs listas en el puesto 3000");
